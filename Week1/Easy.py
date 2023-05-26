d403df603d96ee41b2c665c1656e9ffe250747c9
Ronak De <114191163+Ronak-De@users.noreply.github.com>
2023-05-26 23:35:11 +0100

    attempt2

--- Week1/Easy.py
+++ Week1/Easy.py
@@ -1,3 +1,11 @@
 #Easy 
 #Check whether a string is the same forwards and backwards (Palendrome)
 string=str(input("Enter a string to check whether it is a palendrome "))
+#Size of 1st half of string
+halflen=int(len(string)/2)
+#1st half of string
+half1=string[0:halflen]
+#2nd half of string
+half2=string[halflen:-1]
+print(half1)
+print(half2)
