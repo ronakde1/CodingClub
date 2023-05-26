9a956237602b71bcd71e76a64b87a9c0f2da9198
Ronak De <114191163+Ronak-De@users.noreply.github.com>
2023-05-26 23:59:19 +0100

    Easy attempt 1

--- Week1/Easy.py
+++ Week1/Easy.py
@@ -1,11 +1,31 @@
 #Easy 
 #Check whether a string is the same forwards and backwards (Palendrome)
-string=str(input("Enter a string to check whether it is a palendrome "))
+string=str(input("Enter a string to check whether it is a palendrome ")).lower()
 #Size of 1st half of string
-halflen=int(len(string)/2)
+halflen=len(string)//2
 #1st half of string
 half1=string[0:halflen]
 #2nd half of string
-half2=string[halflen:-1]
-print(half1)
-print(half2)
+half2=string[halflen:]
+
+
+check=True
+for i in range(halflen):
+    if half1[i]!=half2[-i-1]: #Checks if each value in the list is the same as the reflected value
+        check=False 
+if check:
+    print("This is a palendrome")
+else:
+    print("This is not a palendrome")
+
+#-------------------------------------------------------------------------
+#Using fancy python:
+string=str(input("Enter a string to check whether it is a palendrome "))
+if string==string[::-1]:
+    print("This is a palendrome")
+else:
+    print("This is not a palendrome")
+#How does this code work: 
+#The first colon : indicates the slicing operation.
+#The second colon : specifies the step value. 
+#In this case, -1 means we are stepping backward through the string. By default, it is 1
